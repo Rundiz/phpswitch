dfb0a79ed7c6b991fe09f228e69e14515dd521ca
ve3 <kawewong@gmail.com>
2022-06-30 14:22:00 +0700

    Moved `IsAdmin()` & `RelaunchAsAdmin()` to `Privilege` class.

---
+++ phpswitch/Libraries/Privilege.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace phpswitch.Libraries
+{
+    /// <summary>
+    /// Windows privilege user class.
+    /// </summary>
+    class Privilege
+    {
+
+
+        /// <summary>
+        /// Get user ID on Linux.
+        /// For admin, check if `getuid() == 0`.
+        /// https://stackoverflow.com/a/58567692/128761 Original source code.
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("libc")]
+        public static extern uint getuid();
+
+
+        /// <summary>
+        /// Check if running as administrator
+        /// https://stackoverflow.com/a/58567692/128761 Original source code.
+        /// </summary>
+        /// <returns>Return true if yes, false if no.</returns>
+        public static bool IsAdmin()
+        {
+            try
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    // if Windows.
+                    WindowsIdentity user = WindowsIdentity.GetCurrent();
+                    WindowsPrincipal principal = new WindowsPrincipal(user);
+                    return principal.IsInRole(WindowsBuiltInRole.Administrator);
+                }
+                else
+                {
+                    // if not Windows.
+                    return getuid() == 0;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// Re-launch as admin.
+        /// https://stackoverflow.com/a/46080075/128761 Original source code.
+        /// </summary>
+        /// <param name="phpversion">The PHP version to switch to.</param>
+        /// <param name="configJson">Path to phpswitch.json config file.</param>
+        /// <param name="verbose">Verbose value will be true or false.</param>
+        public static void RelaunchAsAdmin(string phpversion, FileInfo configJson, bool verbose)
+        {
+            string argumentString = phpversion;
+            if (String.IsNullOrEmpty(configJson?.FullName) == false)
+            {
+                argumentString += " --config-json=\"" + configJson + "\"";
+            }
+            if (verbose == true)
+            {
+                argumentString += " --verbose";
+            }
+
+            ProcessStartInfo Proc = new ProcessStartInfo();
+            Proc.Arguments = argumentString;
+            // https://stackoverflow.com/a/980225/128761 Get executing app .exe original source code.
+            Proc.FileName = Process.GetCurrentProcess().MainModule.FileName;
+            Proc.UseShellExecute = true;
+            Proc.Verb = "runas";
+            Proc.WorkingDirectory = Environment.CurrentDirectory;
+
+            try
+            {
+                Process.Start(Proc);
+                Environment.Exit(0);
+            }
+            catch
+            {
+                throw new Exception("Unable to raise the administrator privilege.");
+            }
+        }
+
+
+    }
+}
--- phpswitch/SubPrograms/App.cs
+++ phpswitch/SubPrograms/App.cs
@@ -1,4 +1,5 @@
 ﻿using phpswitch.Models;
+using phpswitch.Libraries;
 using System;
 using System.Diagnostics;
 using System.IO;
@@ -30,84 +31,6 @@ namespace phpswitch.SubPrograms
 
 
         /// <summary>
-        /// Get user ID on Linux.
-        /// For admin, check if `getuid() == 0`.
-        /// https://stackoverflow.com/a/58567692/128761 Original source code.
-        /// </summary>
-        /// <returns></returns>
-        [DllImport("libc")]
-        public static extern uint getuid();
-
-
-        /// <summary>
-        /// Check if running as administrator
-        /// https://stackoverflow.com/a/58567692/128761 Original source code.
-        /// </summary>
-        /// <returns>Return true if yes, false if no.</returns>
-        private static bool IsAdmin()
-        {
-            try
-            {
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                {
-                    // if Windows.
-                    WindowsIdentity user = WindowsIdentity.GetCurrent();
-                    WindowsPrincipal principal = new WindowsPrincipal(user);
-                    return principal.IsInRole(WindowsBuiltInRole.Administrator);
-                }
-                else
-                {
-                    // if not Windows.
-                    return getuid() == 0;
-                }
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-
-        /// <summary>
-        /// Re-launch as admin.
-        /// https://stackoverflow.com/a/46080075/128761 Original source code.
-        /// </summary>
-        /// <param name="phpversion">The PHP version to switch to.</param>
-        /// <param name="configJson">Path to phpswitch.json config file.</param>
-        /// <param name="verbose">Verbose value will be true or false.</param>
-        private static void RelaunchAsAdmin(string phpversion, FileInfo configJson, bool verbose)
-        {
-            string argumentString = phpversion;
-            if (String.IsNullOrEmpty(configJson?.FullName) == false)
-            {
-                argumentString += " --config-json=\"" + configJson + "\"";
-            }
-            if (verbose == true)
-            {
-                argumentString += " --verbose";
-            }
-
-            ProcessStartInfo Proc = new ProcessStartInfo();
-            Proc.Arguments = argumentString;
-            // https://stackoverflow.com/a/980225/128761 Get executing app .exe original source code.
-            Proc.FileName = Process.GetCurrentProcess().MainModule.FileName;
-            Proc.UseShellExecute = true;
-            Proc.Verb = "runas";
-            Proc.WorkingDirectory = Environment.CurrentDirectory;
-
-            try
-            {
-                Process.Start(Proc);
-                Environment.Exit(0);
-            }
-            catch
-            {
-                throw new Exception("Unable to raise the administrator privilege.");
-            }
-        }
-
-
-        /// <summary>
         /// Run the application.
         /// </summary>
         /// <param name="phpversion">The PHP version to switch to.</param>
@@ -115,11 +38,11 @@ namespace phpswitch.SubPrograms
         /// <param name="verbose">Verbose value will be true or false.</param>
         public void Run(string phpversion, FileInfo configJson, bool verbose)
         {
-            if (IsAdmin() == false)
+            if (Privilege.IsAdmin() == false)
             {
                 if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 {
-                    RelaunchAsAdmin(phpversion, configJson, verbose);
+                    Privilege.RelaunchAsAdmin(phpversion, configJson, verbose);
                 }
                 else
                 {
